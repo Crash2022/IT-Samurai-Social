5c2ad1e5130e795670b141e67089fdd9df7ca21d
Crash <crash555@mail.ru>
2022-08-16 10:30:55 +0500

    delete not required

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-/* import PageTitle from "./components/PageTitle";
-import Accordion from "./components/Accordion"; */
 
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
@@ -12,11 +10,6 @@ function App(props: any) {
   return (
     <div className="wrapper">
     
-        {/* <PageTitle title={"Main"}/>
-        <Accordion titleValue={"menu 1"} menuCollapsed={false}/>
-        <PageTitle title={"Gallery"}/>
-        <Accordion titleValue={"menu 2"} menuCollapsed={true}/> */}
-
         <Header />
         <Navbar />
         <Profile />
