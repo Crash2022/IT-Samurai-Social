dc0b2f0c722deb90e131d62326ba1154573ca009
Crash2022 <crash555@mail.ru>
2022-10-02 12:25:16 +0500

    lesson 56 done

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -2,7 +2,8 @@ import React, {Dispatch} from "react";
 import {ActionsType, RootStateType, UsersArray} from "../../redux/redux-store";
 import {connect} from "react-redux";
 import {followAC, unfollowAC, setCurrentPageAC, setUsersAC} from "../../redux/users-reducer";
-import {UsersAPIClassComponent} from "./UsersAPIClassComponent";
+import axios from "axios";
+import {Users} from "./Users";
 
 export type MapStatePropsType = {
     users: Array<UsersArray>
@@ -48,5 +49,40 @@ const mapDispatchToProps = (dispatch: Dispatch<ActionsType>) => {
     }
 }
 
+export class UsersAPIClassContainer extends React.Component<UsersContainerType> {
+
+    componentDidMount() {
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.onSetUsers(response.data.items);
+                //this.props.setUsersTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onChangePageHandler = (pageNumber: number) => {
+        this.props.onChangePage(pageNumber);
+
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.onSetUsers(response.data.items)
+            });
+    }
+
+    render() {
+        return (
+            <Users users={this.props.users}
+                   pageSize={this.props.pageSize}
+                   totalUsersCount={this.props.totalUsersCount}
+                   currentPage={this.props.currentPage}
+                   onFollow={this.props.onFollow}
+                   onUnfollow={this.props.onUnfollow}
+                   onChangePageHandler={this.onChangePageHandler}
+            />
+        )
+    }
+}
+
 //export default connect(mapStateToProps, mapDispatchToProps)(Users);
-export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIClassComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIClassContainer);
