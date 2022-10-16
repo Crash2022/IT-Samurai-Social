6d090fecf2d8a1d6bfa34fda6926628bd2c3d456
Crash2022 <crash555@mail.ru>
2022-10-16 17:53:22 +0500

    login styles change

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,7 +4,7 @@ import classes from './Login.module.css';
 export const Login = () => {
     return (
         <div className={classes.login}>
-            <h1>Login</h1>
+            <div>Login</div>
         </div>
     );
 }
