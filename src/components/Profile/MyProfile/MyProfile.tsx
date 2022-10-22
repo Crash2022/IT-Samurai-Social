be7117b38de4ad36c981139d1faea90cab814c48
Crash2022 <crash555@mail.ru>
2022-10-22 16:00:57 +0500

    delete unused

--- src/components/Profile/MyProfile/MyProfile.tsx
+++ src/components/Profile/MyProfile/MyProfile.tsx
@@ -14,32 +14,6 @@ type MyProfilePropsType = {
 
 export const MyProfile = (props: MyProfilePropsType) => {
 
-    /*return (
-        <>
-            <div className={classes.content__avatar}>
-                <img src="https://twentysix.ru/uploads/images/00/02/33/2021/07/26/94076d_full.jpg"
-                     alt="profile-avatar"></img>
-            </div>
-
-            <div className={classes.content__info}>
-                <div className={classes.content__info_avatar}>
-                    <img
-                        src="https://i.pinimg.com/736x/c2/6f/23/c26f23951566f65eb495497ccc208fc2--mountain-bike-dark-moon.jpg"
-                        alt="my-avatar"></img>
-                </div>
-                <div className={classes.content__info_info}>
-                    <div>Меня зовут ...</div>
-                    <div>Дата рождения: ...</div>
-                    <div>Город: ...</div>
-                    <div>Хобби: ...</div>
-                    <div>Веб-сайт: ...</div>
-                </div>
-            </div>
-        </>
-    );*/
-
-    //console.log('props', props);
-
     if (!props.profile) {
         return <Preloader/>
     } else {
