a48ccc2a851dfb13b7f693f6aacfa21b352f5647
Crash2022 <crash555@mail.ru>
2022-11-02 00:35:54 +0500

    change pagination styles

--- src/UI/Paginator/Paginator.tsx
+++ src/UI/Paginator/Paginator.tsx
@@ -12,51 +12,61 @@ export type PaginatorPropsType = {
 export const Paginator = (props: PaginatorPropsType) => {
 
     const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize); // всего страниц
-    const pages = [];
+    const pages = []; // массив со страницами
 
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
-    const numberOfPages = 15; // количество страниц с пользователями на одной странице
-    const [partOfPages, setPartOfPages] = useState<number>(1);
-    const partOfPagesCount = Math.ceil(pagesCount/numberOfPages);
-    const prevPageNumber = (partOfPages-1)*numberOfPages+1;
-    const nextPageNumber = partOfPages*numberOfPages;
+    const numberOfPages = 10; // количество страниц с пользователями на одной странице
+    const [partOfPages, setPartOfPages] = useState<number>(1); // для вычисления блока страниц
+    const partOfPagesCount = Math.ceil(pagesCount / numberOfPages); // количество блоков со страницами
+    const prevPageNumber = (partOfPages - 1) * numberOfPages + 1;
+    const nextPageNumber = partOfPages * numberOfPages;
 
     return (
         <>
-            <div className={styles.pagination}>
-                <button onClick={() => {props.onChangePageHandler(props.currentPage-1)}}
-                        disabled={props.currentPage === 1}
-                >
-                    Пред.
-                </button>
-            </div>
-            <div className={styles.pagination}>
-                {
-                    pages
-                        .filter(page => page > prevPageNumber && page <= nextPageNumber)
-                        .map(page => {
-                            return (
-                                <span className={props.currentPage === page ? styles.selectedPage : ''}
-                                      key={v1()}
-                                      onClick={() => {
-                                          props.onChangePageHandler(page)
-                                      }}
-                                >
+            <div className={styles.paginationWrapper}>
+                <div className={styles.paginationButtonPrev}>
+                    <button onClick={() => {
+                        //props.onChangePageHandler(props.currentPage - 1)
+                        setPartOfPages(partOfPages-1)
+                    }}
+                            //disabled={props.currentPage === 1}
+                    >
+                        Пред.
+                    </button>
+                </div>
+
+                <div className={styles.pagination}>
+                    {
+                        pages
+                            .filter(page => page >= prevPageNumber && page <= nextPageNumber)
+                            .map(page => {
+                                return (
+                                    <span className={props.currentPage === page ? styles.selectedPage : ''}
+                                          key={v1()}
+                                          onClick={() => {
+                                              props.onChangePageHandler(page)
+                                          }}
+                                    >
                                   {page}
                             </span>
-                            )
-                    })
-                }
-            </div>
-            <div className={styles.pagination}>
-                <button onClick={() => {props.onChangePageHandler(props.currentPage+1)}}
-                        //disabled={props.currentPage === pagesCount}
-                >
-                    След.
-                </button>
+                                )
+                            })
+                    }
+                </div>
+
+                <div className={styles.paginationButtonNext}>
+                    <button onClick={() => {
+                        //props.onChangePageHandler(props.currentPage + 1)
+                        setPartOfPages(partOfPages+1)
+                    }}
+                            //disabled={props.currentPage === pagesCount}
+                    >
+                        След.
+                    </button>
+                </div>
             </div>
         </>
     );
